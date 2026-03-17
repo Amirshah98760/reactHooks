51d36fb381da1944cd254b09746f028e6a5a6dc8
Amirshah98760 <amirshah98760@gmail.com>
2026-03-17 14:41:52 +0500

    Create a simple counter app

--- reactHooks/src/App.jsx
+++ reactHooks/src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  if(count < 0){
+    setCount(0)
+  }
+
   return (
-   <div>
-      <p>Hello React Hooks Repository 🚀</p>
+   <div className='mainContainer'>
+   <div className='container'>
+    <h2>React Hooks</h2>
+    <h2>Counter :  {count}</h2>
+
+    <div className='btnDiv'>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement </button>
+      <button onClick={reset}>Reset</button>
+    </div>
+   </div>
     </div>
   )
 }
